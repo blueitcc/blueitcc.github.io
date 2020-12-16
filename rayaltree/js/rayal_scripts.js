9606069f078fcb2189f70b42ea6f5ce73168f2b1
blueitcc <49081972+blueitcc@users.noreply.github.com>
2020-12-16 12:13:07 -0500

    Add files via upload

---
+++ rayaltree/js/rayal_scripts.js
@@ -0,0 +1,77 @@
+var mr_firstSectionHeight,
+    mr_nav,
+    mr_fixedAt,
+    mr_navOuterHeight,
+    mr_navScrolled = false,
+    mr_navFixed = false,
+    mr_outOfSight = false,
+    mr_floatingProjectSections,
+    mr_scrollTop = 0;
+
+$(document).ready(function() { 
+    "use strict";
+
+
+    var canvas = document.querySelector("canvas");
+
+
+    var signaturePad = new SignaturePad(canvas, {
+        // It's Necessary to use an opaque color when saving image as JPEG;
+        // this option can be omitted if only saving as PNG or SVG
+        backgroundColor: 'rgb(255, 255, 255)'
+      });
+    
+
+
+function resizeCanvas() {
+    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext("2d").scale(ratio, ratio);
+    signaturePad.clear(); // otherwise isEmpty() might return incorrect value
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+    var form = $('form#submission-form'),
+    url = 'https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbx_6zACj4BsRiH4a7wzi3jYC_yj8abzmWPFYVaFOgJ8Zxdl_e_A/exec';
+
+$('#clear-signature').on('click', function(e) {
+    e.preventDefault();
+    // Clears the canvas
+    signaturePad.clear();
+})
+
+
+    
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+
+    var signatureData = signaturePad.toDataURL();
+  	$('#signature').val(signatureData);
+
+  var jqxhr = $.ajax({
+    url: url,
+    method: "POST",
+    dataType: "application/x-www-form-urlencoded",
+    data: form.serializeObject()
+  }).success(function() {
+
+    $("#submission-form").html( '<h3 class="text-center">Your Submission has been recorded. Refresh the page to start a new form.</h3>' );
+    
+
+  }).error(function() {
+    $("#submission-form").html( '<h3 class="text-center">Your Submission has been recorded. Refresh the page to start a new form.</h3>' );
+  });
+})
+    
+
+
+
+
+
+   
+
+}); 
+

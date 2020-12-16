26d0eae35171b40d0eef9eeecc6f1d46d2c00f4b
blueitcc <49081972+blueitcc@users.noreply.github.com>
2020-12-16 10:59:46 -0500

    Add files via upload

---
+++ rayaltree/js/rayal_scripts.js
@@ -0,0 +1,97 @@
+var mr_firstSectionHeight,
+    mr_nav,
+    mr_fixedAt,
+    mr_navOuterHeight,
+    mr_navScrolled = false,
+    mr_navFixed = false,
+    mr_outOfSight = false,
+    mr_floatingProjectSections,
+    mr_scrollTop = 0;
+
+$(document).ready(function() { 
+    "use strict";
+
+
+    // $('input[type="checkbox"]').click(function(){
+    //     if($(this).prop("checked") == true){
+    //         console.log("Checkbox is checked.");
+    //         $('#submit-form').removeAttr('disabled');
+            
+    //     }
+    //     else if($(this).prop("checked") == false){
+    //         console.log("Checkbox is unchecked.");
+            
+    //         $('#submit-form').attr('disabled', 'disabled');
+    //     }
+    // });
+
+    var canvas = document.querySelector("canvas");
+
+
+    var signaturePad = new SignaturePad(canvas, {
+        // It's Necessary to use an opaque color when saving image as JPEG;
+        // this option can be omitted if only saving as PNG or SVG
+        backgroundColor: 'rgb(255, 255, 255)'
+      });
+    
+
+
+function resizeCanvas() {
+    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext("2d").scale(ratio, ratio);
+    signaturePad.clear(); // otherwise isEmpty() might return incorrect value
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+
+
+
+
+
+
+    var form = $('form#submission-form'),
+    url = 'https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbx_6zACj4BsRiH4a7wzi3jYC_yj8abzmWPFYVaFOgJ8Zxdl_e_A/exec';
+
+$('#clear-signature').on('click', function(e) {
+    e.preventDefault();
+    // Clears the canvas
+    signaturePad.clear();
+})
+
+
+    
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+
+    var signatureData = signaturePad.toDataURL();
+  	$('#signature').val(signatureData);
+
+  var jqxhr = $.ajax({
+    url: url,
+    method: "POST",
+    dataType: "json",
+    data: form.serializeObject()
+  }).success(function() {
+    $("#submission-form").css("top", "40%");
+    $("#submission-form").html( "<h3>Your Submission has been recorded. Refresh the page to start a new form.</h3>" );
+    
+
+  }).error(function() {
+    $("#form-container").css("top", "40%");
+    $("#submission-form").html( "<h3>There was an error with your Submission. Please contact your adminstrator</h3>" );
+  });
+})
+    
+
+
+
+
+
+   
+
+}); 
+
